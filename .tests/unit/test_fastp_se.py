4156542e71ddfab7b80be2767c3a265495ec7947
nikostr <nikostr@users.noreply.github.com>
2021-04-29 15:27:15 +0200

    Adds tests fo fastp

---
+++ .tests/unit/test_fastp_se.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_fastp_se():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/fastp_se/data")
+        expected_path = PurePosixPath(".tests/unit/fastp_se/expected")
+        config_path = PurePosixPath(".tests/unit/config")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+        shutil.copytree(config_path, workdir / "config" )
+
+        # dbg
+        print("results/trimmed/B-2.fastq.gz results/qc/fastp/B-2.html results/qc/fastp/B-2.json", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "results/trimmed/B-2.fastq.gz",
+            "-F", 
+            "-j1",
+            "--keep-target-files",
+    
+            "--use-conda",
+            "--use-singularity",
+            "--directory",
+            workdir,
+        ])
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
