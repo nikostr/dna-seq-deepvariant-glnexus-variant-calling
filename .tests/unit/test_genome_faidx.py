a1a813adae503bcb369f69df20fe9deda353fff2
nikostr <nikostr@users.noreply.github.com>
2021-04-29 16:46:07 +0200

    Adds tests for deepvariant, faidx, samtools merge

---
+++ .tests/unit/test_genome_faidx.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_genome_faidx():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/genome_faidx/data")
+        expected_path = PurePosixPath(".tests/unit/genome_faidx/expected")
+        config_path = PurePosixPath(".tests/unit/config")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+        shutil.copytree(config_path, workdir / "config" )
+
+        # dbg
+        print("resources/genome.fasta.fai", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "resources/genome.fasta.fai",
+            "-j1",
+            "--keep-target-files",
+    
+            "--use-conda",
+            "--use-singularity",
+            "--directory",
+            workdir,
+        ])
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
---
+++ .tests/unit/test_samtools_merge.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_samtools_merge():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/samtools_merge/data")
+        expected_path = PurePosixPath(".tests/unit/samtools_merge/expected")
+        config_path = PurePosixPath(".tests/unit/config")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+        shutil.copytree(config_path, workdir / "config" )
+
+        # dbg
+        print("results/mapped/A.bam results/mapped/A.bam.csi", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "results/mapped/A.bam",
+            "-j1",
+            "--keep-target-files",
+    
+            "--use-conda",
+            "--use-singularity",
+            "--directory",
+            workdir,
+        ])
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
