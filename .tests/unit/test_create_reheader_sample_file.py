df49af878e91f9de878077d69ec58ef69602020b
nikostr <nikostr@users.noreply.github.com>
2021-04-30 12:08:25 +0200

    Adds create_reheader_sample_file test

---
+++ .tests/unit/test_create_reheader_sample_file.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_create_reheader_sample_file():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/create_reheader_sample_file/data")
+        expected_path = PurePosixPath(".tests/unit/create_reheader_sample_file/expected")
+        config_path = PurePosixPath(".tests/unit/config")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+        shutil.copytree(config_path, workdir / "config")
+
+        # dbg
+        print("results/joint_calls/G1_sample_names.tsv", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output(
+            [
+                "python",
+                "-m",
+                "snakemake",
+                "results/joint_calls/G1_sample_names.tsv",
+                "-j1",
+                "--keep-target-files",
+                "--use-conda",
+                "--conda-frontend",
+                "mamba",
+                "--use-singularity",
+                "--directory",
+                workdir,
+            ]
+        )
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file),
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
